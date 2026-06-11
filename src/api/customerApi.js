2b3752132afad8a73f42b7bcec9165b44cb8e7b2
AanchalGupta0502 <aanchalgupta0205@gmail.com>
2026-06-11 17:57:26 +0530

    Added render url for backend url

--- src/api/customerApi.js
+++ src/api/customerApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL="https://erp-backend-springboot-1.onrender.com"
+const BASE_URL="https://erp-backend-springboot-1.onrender.com/api/customers"
 //const BASE_URL="http://localhost:8080/api/customers";
 
 export const createCustomer=(data)=> axios.post(BASE_URL,data);
