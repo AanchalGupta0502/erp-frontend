2ffce1ce88a8e2a12ae45131f19077a7aeb3be4f
AanchalGupta0502 <aanchalgupta0205@gmail.com>
2026-06-30 23:32:25 +0530

    Added render url to customerReportApi

--- src/api/customerReportApi.js
+++ src/api/customerReportApi.js
@@ -1,5 +1,6 @@
 import axios from "axios"
-const BASE_URL="http://localhost:8080/api/customerReport"
+const BASE_URL="https://erp-backend-springboot-1.onrender.com/api/customerReport"
+//const BASE_URL="http://localhost:8080/api/customerReport"
 
 export const getCustomerByAroma=(aroma)=>axios.get(`${BASE_URL}/${aroma}`);
 export const getCustomerByAromaPDF=(aroma)=>axios.get(`${BASE_URL}/export/pdf/${aroma}`, { responseType: 'blob' });
