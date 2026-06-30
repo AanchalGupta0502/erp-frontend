d2b58b7b646189ec6db8bc1159350ed6c0b2a7ce
AanchalGupta0502 <aanchalgupta0205@gmail.com>
2026-06-30 22:58:52 +0530

    Made axios api for customer report

---
+++ src/api/customerReportApi.js
@@ -0,0 +1,5 @@
+import axios from "axios"
+const BASE_URL="http://localhost:8080/api/customerReport"
+
+export const getCustomerByAroma=(aroma)=>axios.get(`${BASE_URL}/${aroma}`);
+export const getCustomerByAromaPDF=(aroma)=>axios.get(`${BASE_URL}/export/pdf/${aroma}`, { responseType: 'blob' });
